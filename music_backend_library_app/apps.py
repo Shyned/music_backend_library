accc2ab9ccd4aef3575f1eb9dfde5a931ceb3d96
Dyshine Hill <eyedolarts@gmail.com>
2022-04-25 14:51:51 -0500

    created app + created mysql database

---
+++ music_backend_library_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MusicBackendLibraryAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'music_backend_library_app'
